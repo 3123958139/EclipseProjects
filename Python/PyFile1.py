b9313b010290f07f741cd4964d49b4bdfa5446c4
3123958139 <3123958139@qq.com>
2018-06-19 21:03:25 +0800

    add

--- Python/PyFile1.py
+++ Python/PyFile1.py
@@ -9,7 +9,7 @@ path = 'D:\\Program Files\\eclipse-cpp-oxygen-3a-win32-x86_64\\tmp\\EclipseProje
 df = ts.get_k_data('002237')  # tushare的数据接口get_k_data
 df['date'] = df['date'].apply(
     lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))  # 日期由string转成datetime
-df.to_csv("002237.csv", index=False)  # 保存数据到本地csv
+df.to_csv("PyFile1.csv", index=False)  # 保存数据到本地csv
 df.set_index('date', inplace=True)  # 将日期设为index便于画图
 dfPlot = df.ix[:, :-2]  # 画图OHLC
 if __name__ == '__main__':
